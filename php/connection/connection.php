bc0f6ffedc6ed97942c7d7af2f0d03de998523c0
YednapNibas <pandeyfcb7849@gmail.com>
2019-03-02 07:20:21 +0545

    files added to gitignore

--- php/connection/connection.php
+++ php/connection/connection.php
@@ -1,8 +1,8 @@
 <?php
-	define("DB_HOST", "");
-	define("DB_USER", "");
+	define("DB_HOST", "localhost");
+	define("DB_USER", "root");
 	define("DB_PASSWORD", "");
-	define("DB_DATABASE", "");
+	define("DB_DATABASE", "php_futsal");
 
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
     
